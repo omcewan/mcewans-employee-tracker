c9a32b243d956aedb8c4441041cb724f0ef36cd6
omcewan <orlandomcewan231@gmail.com>
2022-02-26 16:50:21 -0500

    added the roles tables and data into roles, also associate the roles to departments

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,16 @@
-DROP TABLE IF EXISTS department;
--- CREATE TABLE roles (
---   id INTEGER AUTO_INCREMENT PRIMARY KEY,
---   title VARCHAR(50) NOT NULL,
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
 
--- )
-CREATE TABLE department (
+CREATE TABLE departments (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(50) NOT NULL
-)
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  department_id INTEGER,
+  salary INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO department (department_name)
+INSERT INTO departments (department_name)
 VALUES 
   ("Human Resources"),
   ("Finance"),
@@ -8,3 +8,22 @@ VALUES
   ("Engineering"),
   ("Maintenance"),
   ("Shipping & Recieving");
+
+INSERT INTO roles (title, department_id, salary)
+VALUES
+  ("Human Resource Manager", 1, 60000),
+  ("Human Resource Assistant", 1, 40000),
+  ("Finance Manager", 2, 70000),
+  ("Finance Assistant", 2, 50000),
+  ("Inorganic Production Manager", 3, 80000),
+  ("Inorganic Production Team Memeber", 3, 60000),
+  ("Organic Production Manager", 4, 80000),
+  ("Organic Production Team Member", 4, 60000),
+  ("Sales Manager", 5, 60000),
+  ("Sales Team Membmer", 5, 40000),
+  ("Engineering Manager", 6, 80000),
+  ("Engineering Team Memeber", 6, 60000),
+  ("Maintenance Manager", 7, 70000),
+  ("Maintenance Team Memeber", 7, 50000),
+  ("Shipping & Recieving Manager", 8, 60000),
+  ("Shipping & Recieving Team Memeber", 8, 40000);
