252645cbfb637b64994009ab989fde8af5c50ea6
omcewan <orlandomcewan231@gmail.com>
2022-02-27 18:42:54 -0500

    added a function to update employee's manager

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const {
   getEmployees,
   employeesByManager,
   employeesByDepartment,
+  updateEmployee,
 } = require("./utils/employees");
 const getRoles = require("./utils/roles");
 const getDepartments = require("./utils/departments");
+const { up } = require("inquirer/lib/utils/readline");
 
 const initialQuestion = [
   {
@@ -26,6 +28,36 @@ const initialQuestion = [
   },
 ];
 
+const updateEmployeeQuestion = [
+  {
+    type: "input",
+    name: "employee",
+    message: "Please enter the ID for the employee you would like to update!",
+    validate: (employee) => {
+      if (employee) {
+        return true;
+      } else {
+        console.log("Please enter an ID");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "manager",
+    message:
+      "Please enter the ID for the manager you would like to assign the employee to!",
+    validate: (manager) => {
+      if (manager) {
+        return true;
+      } else {
+        console.log("Please enter a value!");
+        return false;
+      }
+    },
+  },
+];
+
 const promptUser = () => {
   return inquirer.prompt(initialQuestion).then(({ company }) => {
     if (company === "View All Employees") {
@@ -36,6 +68,11 @@ const promptUser = () => {
     }
     if (company === "View Employees By Department") {
       employeesByDepartment();
+    }
+    if (company === "Update Employee's Manager") {
+      inquirer.prompt(updateEmployeeQuestion).then(({ employee, manager }) => {
+        updateEmployee(employee, manager);
+      });
     }
     if (company === "View All Roles") {
       getRoles();
--- utils/employees.js
+++ utils/employees.js
@@ -19,8 +19,8 @@ async function getEmployees() {
   LEFT JOIN employees M
   ON M.id = E.manager_id`;
 
-  const [rows] = await connection.execute(sql);
-  const allEmployees = cTable.getTable(rows);
+  const [results] = await connection.execute(sql);
+  const allEmployees = cTable.getTable(results);
   console.log(allEmployees);
   connection.end();
 }
@@ -44,8 +44,8 @@ async function employeesByManager() {
   ON M.id = E.manager_id
   ORDER BY manager`;
 
-  const [rows] = await connection.execute(sql);
-  const employeesByManager = cTable.getTable(rows);
+  const [results] = await connection.execute(sql);
+  const employeesByManager = cTable.getTable(results);
   console.log(employeesByManager);
   connection.end();
 }
@@ -69,10 +69,50 @@ async function employeesByDepartment() {
   ON M.id = E.manager_id
   ORDER BY department`;
 
-  const [rows] = await connection.execute(sql);
-  const employeesByDepartment = cTable.getTable(rows);
+  const [results] = await connection.execute(sql);
+  const employeesByDepartment = cTable.getTable(results);
   console.log(employeesByDepartment);
   connection.end();
 }
 
-module.exports = { getEmployees, employeesByManager, employeesByDepartment };
+async function updateEmployee(employee, manager) {
+  const connection = await mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "mountain_DECANT2whitish",
+    database: "my_company",
+  });
+
+  const sql = `UPDATE employees
+  SET manager_id = ${manager}
+  WHERE id = ${employee} `;
+
+  const [results] = await connection.execute(sql);
+  if (!results.affectedRows) { 
+    console.log(results)
+    console.log({ message: "Employee ID Does Not Exist!" });
+  } else {
+    const sql = `SELECT E.id, E.first_name, E.last_name, roles.title AS title,
+    departments.department_name AS department, roles.salary, M.first_name AS manager
+    FROM employees E
+    LEFT JOIN roles 
+    ON E.role_id = roles.id
+    LEFT JOIN departments
+    ON roles.department_id = departments.id
+    LEFT JOIN employees M
+    ON M.id = E.manager_id
+    ORDER BY manager`;
+
+    const [results] = await connection.execute(sql);
+    const employeesByManager = cTable.getTable(results);
+    console.log(employeesByManager);
+  }
+  connection.end();
+}
+
+module.exports = {
+  getEmployees,
+  employeesByManager,
+  employeesByDepartment,
+  updateEmployee,
+};
