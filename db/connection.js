65ea57dd0d5a418fd3044c6ab37dfd90bba70e56
omcewan <orlandomcewan231@gmail.com>
2022-02-27 15:12:00 -0500

    added functions to get all employees, departments and roles'

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,1 @@
-const mysql = require("mysql2");
 
-const db = mysql.createConnection(
-  {
-    host: "127.0.0.1",
-    user: "root",
-    password: "",
-    database: "my_company",
-  },
-  console.log("Connected to the my_company database.")
-);
-
-module.exports = db;
