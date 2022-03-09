76ec1ed0a5338a1bacf8bc9869f68c7c5a76579a
Orlando McEwan <92196574+omcewan@users.noreply.github.com>
2022-03-09 09:33:27 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection({
+  // If connection error occurs change host from 'localhost' to your IP address
   host: 'localhost',
   user: 'root',
   password: 'mountain_DECANT2whitish',
