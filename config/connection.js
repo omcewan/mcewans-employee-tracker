7d26f4c41f98efcc30881f156a47d4c811c89425
omcewan <orlandomcewan231@gmail.com>
2022-03-09 16:12:38 -0500

    change user for mysql to allow for remote connection

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createConnection({
   // If connection error occurs change host from 'localhost' to your IP address
-  host: 'localhost',
-  user: 'root',
-  password: 'mountain_DECANT2whitish',
+  host: '127.0.0.1',
+  user: 'msu',
+  password: 'CERVIX2nurse-leal',
   database: 'my_company',
 });
 
